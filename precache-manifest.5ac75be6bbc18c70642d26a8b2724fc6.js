89ac60acd1a4a46c0e803eca28dc42e18599545d
admin <body8280@163.com>
2020-01-14 11:38:59 +0800

    add vue

---
+++ precache-manifest.5ac75be6bbc18c70642d26a8b2724fc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748fe0c9f8d40ebd9078",
+    "url": "/css/app.136f6dc0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a6fec06bfaef1714ecc2c888b76843f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b67f43fbe1bbda25c6",
+    "url": "/js/about.b64bab25.js"
+  },
+  {
+    "revision": "748fe0c9f8d40ebd9078",
+    "url": "/js/app.9dad5a18.js"
+  },
+  {
+    "revision": "efe648bd4fd72e76bc66",
+    "url": "/js/chunk-vendors.589739c8.js"
+  },
+  {
+    "revision": "ac7c1195c3b0b913343ab064cedde761",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
